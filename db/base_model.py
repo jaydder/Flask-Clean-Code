cd00ed98134628fcbba470853cf5939db9d441ea
jaydder <jaydderfavoravel@gmail.com>
2024-10-30 09:59:40 -0300

    db.base_model: change connection type to postgres db

--- db/base_model.py
+++ db/base_model.py
@@ -1,5 +1,10 @@
-import peewee
+from peewee import PostgresqlDatabase, Model
+import os
 
-class BaseModel(peewee.Model):
+
+class BaseModel(Model):
     class Meta:
-        database = peewee.SqliteDatabase('sales.db')
+       database = PostgresqlDatabase('user',
+                                    user=os.environ.get('POSTGRES_USER'),
+                                    password=os.environ.get('POSTGRES_PASSWORD'),
+                                    host=os.environ.get('POSTGRES_HOST'))
