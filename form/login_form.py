d5988cfdcd8ac44c2cac4d6832f9cbf915a2917d
jaydder <jaydderfavoravel@gmail.com>
2024-11-01 07:57:29 -0300

    form: added form files

---
+++ form/login_form.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(),], render_kw={"placeholder": "Username"})
+    password = PasswordField('Password', validators=[DataRequired()], render_kw={"placeholder": "Password"})
+    submit = SubmitField('Sign In')
